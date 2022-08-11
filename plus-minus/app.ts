a65f3ffa938c0f75ad804e496312d78fdcfc1cf7
Nathan Skiles <nateskiles94@gmail.com>
2022-08-10 23:12:02 -0700

    add desc

--- plus-minus/app.ts
+++ plus-minus/app.ts
@@ -3,7 +3,7 @@
  *
  * Given an array of integers, calculate the ratios of its elements that are positive, 
  * negative, and zero. Print the decimal value of each fraction on a new line with  
- * places after the decimal.
+ * 6 places after the decimal.
  * 
  * The function accepts INTEGER_ARRAY arr as parameter.
  */

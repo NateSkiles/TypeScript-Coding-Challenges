90f0e3a2ca53e5893689c2dc31851cb816de37c4
Nathan Skiles <nateskiles94@gmail.com>
2022-08-10 23:10:18 -0700

    add desc

--- plus-minus/app.ts
+++ plus-minus/app.ts
@@ -1,6 +1,10 @@
 /*
  * Complete the 'plusMinus' function below.
  *
+ * Given an array of integers, calculate the ratios of its elements that are positive, 
+ * negative, and zero. Print the decimal value of each fraction on a new line with  
+ * places after the decimal.
+ * 
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
 
